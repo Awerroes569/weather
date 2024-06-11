d548f8a7baf7a3f8771675c10095aec83bde608a
AS <arssoo569@gmail.com>
2024-06-11 16:21:52 +0200

    first commit

---
+++ src/components/PickCity/PickCity.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import styles from './PickCity.module.scss';
+
+const PickCity = ({ searching }) => {
+  const [city, setCity] = useState('');
+
+  const searchingCity = (e) => {
+    e.preventDefault();
+    console.log('city', city);
+    searching(city);
+  };
+
+  return (
+    <form className={styles.pickCityForm} onSubmit={searchingCity}>
+      <label>
+        <TextInput
+          placeholder="Enter city name...."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </label>
+      <Button onClick={searchingCity}>Search</Button>
+    </form>
+  );
+};
+
+export default PickCity;
